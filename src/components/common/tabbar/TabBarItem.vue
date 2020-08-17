445712c47e8acc5bd38e649c760b42aa5b4171ee
lihaohan <1035167212@qq.com>
2020-08-17 18:34:14 +0800

    tabbar

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="tab-bar-item">
+    <div class="item-icon"><slot name="icon" /></div>
+    <div class="item-active-icon"><slot name="icon-active" /></div>
+    <div class="item-text"><slot name="text" /></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#tab-bar-item {
+  flex: 1;
+}
+.item-icon img,
+.item-active-icon img {
+  width: 24px;
+  height: 24px;
+  margin-top: 5px;
+  vertical-align: middle;
+}
+.item-text {
+  font-size: 12px;
+  margin-top: 3px;
+  color: #333;
+}
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <TabBar>
+      <TabBarItem>
+        <img slot="icon" src="" alt="" />
+        <div slot="text">首页</div>
+      </TabBarItem>
+      <TabBarItem>
+        <img slot="icon" src="" alt="" />
+        <div slot="text">分类</div>
+      </TabBarItem>
+      <TabBarItem>
+        <img slot="icon" src="" alt="" />
+        <div slot="text">购物车</div>
+      </TabBarItem>
+      <TabBarItem>
+        <img slot="icon" src="" alt="" />
+        <div slot="text">我的</div>
+      </TabBarItem>
+    </TabBar>
+  </div>
+</template>
+
+<script>
+import TabBar from "common/tabbar/TabBar.vue";
+import TabBarItem from "common/tabbar/TabBarItem.vue";
+
+export default {
+  components: {
+    TabBar,
+    TabBarItem
+  }
+};
+</script>
+
+<style scoped></style>
